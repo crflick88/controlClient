de1baf54967a6683f20b5e1ad27086fa2aac8704
crflick <crflick88@gmail.com>
2016-03-31 06:41:53 -0700

    user routes work around

--- src/login/login.js
+++ src/login/login.js
@@ -9,7 +9,7 @@ export class Login {
 
   // User inputs will be bound to these view models
   // and when submitting the form for login  
-  username = '';
+  email = '';
   password = '';
 
   // This view model will be given an error value
@@ -21,7 +21,12 @@ export class Login {
   };
 
   login() {
-    return this.auth.login(this.username, this.password)
+      console.log("email: "+this.email);
+      
+      console.log("password: "+this.password);
+      
+      
+    return this.auth.login(this.email, this.password)
     .then(response => {
       //console.log("Login response content: " + response.content);
       console.log("Login response token: " + response.content.id_token);
